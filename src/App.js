29059f45a023a745a93a6e770a9def701549df79
vgutta <vineethg@udel.edu>
2018-05-31 12:15:48 -0400

    changed default

--- src/App.js
+++ src/App.js
@@ -1,21 +1,96 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
+import _ from 'lodash';
+import currencies from './currencies.json';
+import { Line, Chart} from 'react-chartjs-2';
+import moment from 'react-moment';
+
+const BITCOIN_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          
-        </p>
-      </div>
-    );
+  constructor(props){
+    super(props)
   }
-de }
+
+  componentDidMount () {
+    this.getBitcoinData()
+  }
+
+  getBitcoinData () {
+    fetch(`https://api.coindesk.com/v1/bpi/historical/close.json?currency=${this.state.currency}`)
+    .then(response => response.json)
+    .then(historicalData => this.setState({historicalData}))
+    .catch(e => e)
+  }
+
+  formatChartData () {
+    const {bpi} = this.state.historicalData
+
+    return {
+      labels: _.map(_.keys(bpi), date => moment(date).format("ll")),
+      datasets: [
+        {
+          label: "Bitcoin",
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: _.values(bpi)
+        }
+      ]
+    }
+  }
+
+  setCurrency (currency) {
+    this.setState({currency}, this.getBitcoinData)
+  }
+
+  onCurrencySelect (e) {
+    this.setCurrency(e.target.value)
+  }
+
+  render() {
+    if (!!this.state.historicalData) {
+      return (
+        <div className="app">
+          <Header title="Bitcoin Price Index"/>
+
+          <div className="select-container">
+            <span style={{fontSize: 18, fontFamily: 'Bungee'}}>Select your currency: </span>
+            <select value={this.state.currency} onChange={this.onCurrencySelect}>
+              {currencies.map((obj, index) =>
+                <option key={`${index}-${obj.country}`} value={obj.currency}> {obj.currency} </option>
+              )}
+            </select>
+            {
+              this.state.currency !== 'PHP' && (<div>
+                <a href="#" className="link" onClick={() => this.setCurrency('PHP')} style={{color: "black", fontSize: 16, fontFamily: 'Bungee'}}> [CLICK HERE TO RESET] </a>
+              </div>)
+            }
+          </div>
+
+          <div style={{marginTop: 10}}>
+            <Line data={this.formatChartData()} height={250} />
+          </div>
+        </div>
+      )
+    }
+    return null
+  }
+}
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const Header = props => (
+    <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          
+        </p>
+      </div>
+)
+
+export default Header
